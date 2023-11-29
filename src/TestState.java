5ddc589b53e7d4314f6aa9df1a277ed24fffa561
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2023-11-29 21:28:48 +0800

    State pattern works now

--- src/TestState.java
+++ src/TestState.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class TestState extends State{
+
+    public TestState(BattleSystem bs) {
+        super(bs);
+    }
+
     @Override
     public void Start() {
+        System.out.println("Test: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        bs.setState(new Test2State(bs));
 
     }
 }
