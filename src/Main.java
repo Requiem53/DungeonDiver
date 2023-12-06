9e0f08543125ea1bc04332b16efd603cf152e139
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2023-12-06 16:25:56 +0800

    Equip testing

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,28 @@
 import Characters.Character;
 import Characters.CharacterClass;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         //Add character creator
-
         Character.Ally char1 = new Character.Ally("Maurice", new CharacterClass.Warrior());
-        Character.Ally char2 = new Character.Ally("Jorash", new CharacterClass.Warrior());
-        Character.Ally char3 = new Character.Ally("Van", new CharacterClass.Warrior());
-        Character.Ally char4 = new Character.Ally("Slamm", new CharacterClass.Warrior());
 
-        Character.Enemy enemyTest = new Character.Enemy("Ugang", new CharacterClass.Warrior());
-        Character.Enemy enemyTest2 = new Character.Enemy("Big Mom", new CharacterClass.Warrior());
-        List<Character> characters = new ArrayList<>();
-        characters.add(char1);
-        characters.add(char2);
-        characters.add(char3);
-        characters.add(char4);
-        characters.add(enemyTest);
-        characters.add(enemyTest2);
+        String input;
 
-        BattleSystem bs = new BattleSystem(characters);
+        do{
+            System.out.println("What do: ");
+            input = sc.nextLine();
+
+            switch (input){
+                case "DONE":
+                    break;
+                case "Print Details":
+            }
+
+        } while (!input.equals("DONE"));
+
     }
 }
--- src/WeaponBuilder.java
+++
@@ -1,7 +0,0 @@
-public interface WeaponBuilder {
-    void setMaxHealth(int maxHealth);
-    void setPower(int power);
-    void setSpeed(int speed);
-    void setDefense(int defense);
-    void setMagicPower(int magicPower);
-}
