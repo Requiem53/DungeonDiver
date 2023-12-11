c9a0ac7223005a5a2ed05a6461b652f502e89325
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2023-12-11 20:56:24 +0800

    small change

--- src/States/Shop.java
+++ src/States/Shop.java
@@ -1,5 +1,16 @@
 package States;
 
-public class Shop {
+import GameSystems.BattleSystem;
 
+public class Shop extends State{
+
+    public Shop(BattleSystem bs) {
+        super(bs);
+    }
+
+    @Override
+    public void Start() {
+        System.out.println("This the shop yeah");
+        bs.setState(new DescendLevel(bs));
+    }
 }
