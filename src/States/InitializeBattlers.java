368cd864e254022f76a1b4e05bdee1dcc54710a0
pokemau <rentillosa90@gmail.com>
2023-12-13 22:57:04 +0800

    add starting info on allies and enemies in StatPanel

--- src/States/InitializeBattlers.java
+++ src/States/InitializeBattlers.java
@@ -2,6 +2,9 @@ package States;
 
 import GameSystems.BattleSystem;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class InitializeBattlers extends State{
     //extends State
     //Generates random enemies and descends party member down a level
@@ -18,6 +21,86 @@ public class InitializeBattlers extends State{
         queueChoice.addAll(livingAllies);
         queueChoice.addAll(randomEnemies);
 
+        showAlliesStats();
+        showEnemiesStats();
+
         bs.setState(new BattleStart(bs));
+    }
+
+    private void showAlliesStats() {
+        
+        JPanel statPanel = bs.gameWindow.statPanel;
+
+        for (Characters.Character livingAlly : livingAllies) {
+            JPanel container = new JPanel();
+            container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+            container.setBackground(Color.BLACK);
+            container.setPreferredSize(new Dimension(statPanel.getHeight(), 100));
+
+            JLabel allyName = new JLabel(livingAlly.getName());
+            allyName.setForeground(Color.WHITE);
+            Font existingFont = allyName.getFont();
+            Font newFont = existingFont.deriveFont(18.0f);
+            allyName.setFont(newFont);
+
+            String hpText = "H: " + livingAlly.getCurrHealth() + "/" + livingAlly.getMaxHealth();
+            JLabel hpLabel = new JLabel(hpText);
+            hpLabel.setForeground(Color.WHITE);
+            hpLabel.setFont(newFont);
+
+            String manaText = "M: " + livingAlly.getCurrMana();
+            JLabel manaLabel = new JLabel(manaText);
+            manaLabel.setForeground(Color.WHITE);
+            manaLabel.setFont(newFont);
+
+            container.add(Box.createVerticalGlue());
+            container.add(allyName);
+            container.add(hpLabel);
+            container.add(manaLabel);
+            container.add(Box.createVerticalGlue());
+
+            statPanel.add(container);
+        }
+
+        statPanel.revalidate();
+        statPanel.repaint();
+    }
+
+    private void showEnemiesStats() {
+        JPanel statPanel = bs.gameWindow.statPanel;
+
+        for (Characters.Character enemy : randomEnemies) {
+            JPanel container = new JPanel();
+            container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+            container.setBackground(Color.BLACK);
+            container.setPreferredSize(new Dimension(statPanel.getHeight(), 100));
+
+            JLabel enemyName = new JLabel(enemy.getName());
+            enemyName.setForeground(Color.RED);
+            Font existingFont = enemyName.getFont();
+            Font newFont = existingFont.deriveFont(18.0f);
+            enemyName.setFont(newFont);
+
+            String hpText = "H: " + enemy.getCurrHealth() + "/" + enemy.getMaxHealth();
+            JLabel hpLabel = new JLabel(hpText);
+            hpLabel.setForeground(Color.WHITE);
+            hpLabel.setFont(newFont);
+
+            String manaText = "M: " + enemy.getCurrMana();
+            JLabel manaLabel = new JLabel(manaText);
+            manaLabel.setForeground(Color.WHITE);
+            manaLabel.setFont(newFont);
+
+            container.add(Box.createVerticalGlue());
+            container.add(enemyName);
+            container.add(hpLabel);
+            container.add(manaLabel);
+            container.add(Box.createVerticalGlue());
+
+            statPanel.add(container);
+        }
+
+        statPanel.revalidate();
+        statPanel.repaint();
     }
 }
